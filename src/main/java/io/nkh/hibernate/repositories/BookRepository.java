4cac89c2d063aefd2a051bd0e5e64be1bb2ea5ff
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-11-16 19:12:21 +0330

    add files

--- src/main/java/io/nkh/hibernate/repositories/BookRepository.java
+++ src/main/java/io/nkh/hibernate/repositories/BookRepository.java
@@ -2,7 +2,17 @@ package io.nkh.hibernate.repositories;
 
 
 import io.nkh.hibernate.domain.Book;
+import jakarta.annotation.Nullable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findBookByTitle(String title);
+
+    Book readByTitle(String title);
+
+    @Nullable
+    Book getByTitle(@Nullable String title);
 }
--- src/test/java/io/nkh/hibernate/BookRepositoryTest.java
+++ src/test/java/io/nkh/hibernate/BookRepositoryTest.java
@@ -1,4 +1,42 @@
 package io.nkh.hibernate;
 
+import io.nkh.hibernate.domain.Book;
+import io.nkh.hibernate.repositories.BookRepository;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ActiveProfiles("dev")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ComponentScan(basePackages = {"io.nkh.hibernate.dao"})
+@DataJpaTest
 public class BookRepositoryTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testEmptyResultException() {
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+           Book book = bookRepository.readByTitle("foobar4");
+        });
+    }
+
+    @Test
+    void testNullParam() {
+        assertNull(bookRepository.getByTitle(null));
+    }
+
+    @Test
+    void testNoException() {
+        assertNull(bookRepository.getByTitle("foo"));
+    }
 }
